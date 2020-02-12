f21e0c4815abfef36d71c59ad0d900a5361163c1
Pratik <pratikk@theextraaedge.com>
2020-02-12 13:11:50 +0530

    allowed host

--- HelloWorld/settings.py
+++ HelloWorld/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1%ei)%d&eaatyg=*$i*&x(c&))2a@3a*h+uo=f+frr3ksa&*py'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
